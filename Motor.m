31529d7d1366a538b9cb2c2cdc7dddb9b71d48f2
tanpro260196 <tanpro260196@gmail.com>
2018-11-17 16:42:10 -0800

    How to BS in Matlab 101

---
+++ Motor.m
@@ -0,0 +1,32 @@
+dt = 0.01;
+t = 0:dt:3.5;
+n=3.5/dt;
+J = 0.01;
+b = 0.1;
+K = 0.01;
+R = 1;
+L = 0.5;
+s = tf('s');
+P_motor = K/((J*s+b)*(L*s+R)+K^2);
+
+A = [-b/J   K/J
+    -K/L   -R/L];
+B = [0
+    1/L];
+C = [1   0];
+C_prime = [0 1];
+D = 0;
+motor_ss = ss(A,B,C,D)
+generator_ss = ss(A,B,C_prime,D)
+u(1:1,1:n+1) = 5;
+IC = [0 0];
+alpha = lsim(motor_ss,u,t,IC);
+current = lsim(generator_ss,u,t,IC);
+maxalpha = max(alpha(:));
+maxcurrent = max(current(:));
+figure;
+plot(t,alpha,t, current);
+title('Motor/Generator Output');
+xlabel('Time (s)');
+ylabel('Angular Acceleration/Current');
+legend('Angular Acceleration','Current');
