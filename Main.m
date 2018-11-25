de8a39fda3d783d9e4d186a5011a22748a1d830d
tanpro260196 <tanpro260196@gmail.com>
2018-11-25 13:40:56 -0800

    cmt

--- Main.m
+++ Main.m
@@ -6,5 +6,6 @@ clc;
 T_amb_discrete = [10 13 16 19 23 24 22 20 17 14 10];
 voltage_from_solar = solar(T_amb_discrete); %output voltage from solar panel circuit
 %voltage_from_solar = [11 10 9 8 7 6 5 4 3 2 1];
+
 Motor_output = Motor(voltage_from_solar,50);
 Flywheel_current = Generator(Motor_output(1),Motor_output(2));
--- Motor.m
+++ Motor.m
@@ -1,16 +1,15 @@
-function [maxvalues] = Motor(voltage_from_solar, t_final)
-%MOTOR_FUNCTION Summary of this function goes here
-%   Detailed explanation goes here
+function [endvalue] = Motor(voltage_from_solar, t_final)
 
 
+%All the constant
 m_fly = 1;
 J = m_fly*4e-2;
 b = 0.01;
 K = 0.01;
 R = 1;
 L = 0.5;
-s = tf('s');
 
+%System Matrices
 A = [-b/J   K/J
     -K/L   -R/L];
 B = [0
@@ -18,15 +17,22 @@ B = [0
 C = [1   0];
 C_prime = [0 1];
 D = 0;
+
+%Setup State Space
 alpha_ss = ss(A,B,C,D);
 current_ss = ss(A,B,C_prime,D);
 
 
-
+%Since the input is an array with 11 value of voltage during the time of 11
+%hours. I will run the state space system with each value for 1 hours and
+%combine them all in a big array.
 dt = 0.005;
+%Get the size of the input array
 array_size = size(voltage_from_solar);
+%Initializing all vars.
 alpha = [0];
 current = [0];
+
 for i= 1:array_size(2)
     n = 3600/dt;
     %Input is voltage from Solar.
@@ -35,10 +41,15 @@ for i= 1:array_size(2)
     alpha = [alpha; lsim(alpha_ss,u,0:dt:3600,IC)];
     current = [current; lsim(current_ss,u,0:dt:3600,IC)];
 end
-maxvalues = [alpha(end) current(end)];
+%Grab value at the end of each array and put them in the output.
+endvalue = [alpha(end) current(end)];
+
+%Calculate the time array to plot.
 result_size = size(alpha);
 t_final = (result_size-1) * dt;
 t = 0:dt:t_final;
+
+%Plot
 figure;
 title('Motor Output');
 subplot(1,2,1);
