ea7383e8b073c03937d108014d9eb607f71ba9c3
tanpro260196 <tanpro260196@gmail.com>
2018-11-24 23:12:51 -0800

    The undeniable evident that I suck at Matlab.

--- Main.m
+++ Main.m
@@ -1,4 +1,5 @@
-
+global thermal;
 %Assuming solar output a constant voltage.
-Motor_output = Motor(5);
+voltage_from_solar = linspace(10,0,500);
+Motor_output = Motor(voltage_from_solar,20);
 Flywheel_current = Generator(Motor_output(1),Motor_output(2));
--- Motor.m
+++ Motor.m
@@ -1,12 +1,17 @@
-function [maxvalues] = Motor(voltage_from_solar)
+function [maxvalues] = Motor(voltage_from_solar, t_final)
 %MOTOR_FUNCTION Summary of this function goes here
 %   Detailed explanation goes here
-dt = 0.01;
-t_final = 3.5;
+
+%voltage_from_solar = linspace(10,0,500);
+array_size = size(voltage_from_solar);
+%t_final = 5;
+dt = t_final/(array_size(2)-1);
+
 t = 0:dt:t_final;
-n=t_final/dt;
-J = 0.01;
-b = 0.1;
+n=round(t_final/dt,0);
+m_fly = 2;
+J = m_fly*4e-2;
+b = 0.01;
 K = 0.01;
 R = 1;
 L = 0.5;
@@ -20,8 +25,8 @@ B = [0
 C = [1   0];
 C_prime = [0 1];
 D = 0;
-motor_ss = ss(A,B,C,D)
-generator_ss = ss(A,B,C_prime,D)
+motor_ss = ss(A,B,C,D);
+generator_ss = ss(A,B,C_prime,D);
 %Input is voltage from Solar.
 u(1:1,1:n+1) = voltage_from_solar;
 IC = [0 0];
@@ -30,8 +35,8 @@ current = lsim(generator_ss,u,t,IC);
 maxvalues = [max(alpha(:)) max(current(:))];
 figure;
 plot(t,alpha,t, current);
-title('Motor/Generator Output');
+title('Motor Output');
 xlabel('Time (s)');
-ylabel('Angular Acceleration/Current');
-legend('Angular Acceleration','Current');
+ylabel('Angular Velocity/Current');
+legend('Angular Velocity','Current');
 end
